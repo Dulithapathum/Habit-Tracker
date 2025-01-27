75f0becfb93d498478187c1165a32569e3ff1014
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-01-27 10:37:25 +0530

    Exporting State and DIspatch Types

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import habitsReducer from "./habitSlice";
-export default configureStore({
+const store = configureStore({
   reducer: {
     habits: habitsReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
