611fcd356d9dfb8c89f826944549114d997dc924
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-01-27 11:28:52 +0530

    Create App UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Provider } from "react-redux";
 import Store from "./Store/Store";
+import { Container, Typography } from "@mui/material";
+import AddHabitForm from "./Components/AddHabitForm";
 
 const App = () => {
   return (
     <Provider store={Store}>
-      <div>App</div>
+      <Container maxWidth="md">
+        <Typography component="h1" variant="h2" align="center">
+          Habit Tracker
+        </Typography>
+        <AddHabitForm />
+      </Container>
     </Provider>
   );
 };
