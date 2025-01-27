763a31739617b2098a3020ee943402c11599301f
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-01-27 20:18:16 +0530

    Displaying the Streak

--- src/Components/HabitList.tsx
+++ src/Components/HabitList.tsx
@@ -1,12 +1,37 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../Store/Store";
-import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  LinearProgress,
+  Paper,
+  Typography,
+} from "@mui/material";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import { toggleHabit } from "../Store/habitSlice";
+import { Habit, toggleHabit } from "../Store/habitSlice";
+import { Hvac } from "@mui/icons-material";
 const HabitList: React.FC = () => {
   const { habits } = useSelector((state: RootState) => state.habits);
   const today = new Date().toISOString().split("T")[0];
+
+  const getStreak = (habits: Habit) => {
+    let streak = 0;
+    const currentDate = new Date();
+
+    while (true) {
+      const dateString = currentDate.toISOString().split("T")[0];
+      if (habits.completedDates.includes(dateString)) {
+        streak++;
+        currentDate.setDate(currentDate.getDate() - 1);
+      } else {
+        break;
+      }
+    }
+    return streak;
+  };
+
   const dispatch = useDispatch<AppDispatch>();
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
@@ -56,6 +81,16 @@ const HabitList: React.FC = () => {
                 </Box>
               </Grid>
             </Grid>
+            <Box sx={{ mt: 2 }}>
+              <Typography variant="body2">
+                Current Streak :{getStreak(habit)} Days
+              </Typography>
+              <LinearProgress
+                variant="determinate"
+                value={(getStreak(habit) / 30) * 100}
+                sx={{ mt: 1 }}
+              />
+            </Box>
           </Paper>
         ))
       )}
